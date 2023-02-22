1096c8a556ec39d780feba7bb25403eb05b6ce7a
CesarJZO <cesarjzo@outlook.com>
2023-02-22 08:41:25 -0500

    Add some more whiles

--- while.r
+++ while.r
@@ -5,3 +5,21 @@ while (i <= 5) {
     print(x[i])
     i = i + 1
 }
+
+i = 1
+while (TRUE) {
+    cat("X:", x[i], "\n")
+    i = i + 1
+    if (i == 5)
+        break
+}
+
+
+i = 1
+while (TRUE) {
+    cat("X:", x[i], "\n")
+    i = i + 1
+    if (i == 5)
+        break
+    Sys.sleep(5)
+}
