518070c3ebf0905d67ecfcc5405d42c3ebf94652
CesarJZO <cesarjzo@outlook.com>
2023-02-23 00:19:28 -0500

    Add example using file read, if, for and while

---
+++ example.r
@@ -0,0 +1,16 @@
+# Read a csv file containing integers
+array_data <- scan("/root/R/data.csv", sep=",")
+
+# Print only even numbers
+for (i in array_data) {
+    if (i %% 2 == 0)
+        print(i)
+}
+
+# Print only odd numbers
+i <- 1
+while (i <= length(array_data)) {
+    if (i %% 2 != 0)
+        print(array_data[i])
+    i <- i + 1
+}
