986fadd1638b2322ea741ad3c1bed06f3ab526a5
César Zárate <cesarjzo@outlook.com>
2023-03-02 20:58:09 -0600

    Add gender movie counter

--- march2_exercise/movies_by_gender.r
+++ march2_exercise/movies_by_gender.r
@@ -1,10 +1,23 @@
-# ¿Cuántas películas vieron H y M?
+# 2. v¿Cuántas películas vieron H y M?
 
 library(dplyr)
 
 uuser <- read.table("../sources/u.user", header=TRUE, sep="|")
 udata <- read.table("../sources/udata.data", header=TRUE)
 
-# Get not repeated itemid count grouped by gender
 movies_users <- merge(udata, uuser, by.x="userid", by.y="userid")
+colnames(movies_users)
 
+# get count of column itemid non repeated values
+movies_count <- aggregate(movies_users$userid, by=list(movies_users$itemid), FUN=length)
+colnames(movies_count) <- c("itemid", "count")
+
+# count number of movies per user gender
+movies_count <- merge(movies_count, uuser, by.x="itemid", by.y="userid")
+
+# count itemid where gender is M
+movies_count_m <- movies_count[movies_count$gender == "M",]
+male <- nrow(movies_count_m)
+movies_count_f <- movies_count[movies_count$gender == "F",]
+female <- nrow(movies_count_f)
+cat("\n", male, "movies were watched by men", "\n\n", female, "movies were watched by women", "\n")
--- march2_exercise/top_10_movies.r
+++ march2_exercise/top_10_movies.r
@@ -2,21 +2,25 @@ library(dplyr)
 
 # 1. ¿Cuál es la película más popular? (Top 10)
 
+# read in the data
 udata <- read.table("../sources/udata.data", header=TRUE)
 uitem <- read.table("../sources/u.item", header=TRUE, sep="|")
 
+# merge the data
 movies_users <- merge(udata, uitem, by.x="itemid", by.y="itemid")
-# colnames(movies_users)
+
+# count the number of users per movie
 movies_users_count <- aggregate(movies_users$userid, by=list(movies_users$itemid), FUN=length)
 colnames(movies_users_count) <- c("itemid", "count")
-# colnames(movies_users_count)
 
+# merge the data again
 users_item <- left_join(movies_users, movies_users_count, by=c("itemid"="itemid"))
-# colnames(users_item)
 
+# count the number of users per movie
 users_item <- users_item %>% group_by(title) %>% summarise(users=n_distinct(userid))
-# colnames(users_item)
 
+# sort the data
 users_item <- users_item[order(-users_item$users),]
 
+# get the top 10
 users_item[1:10,]
