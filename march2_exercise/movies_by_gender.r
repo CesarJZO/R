1d2604284495cebec13b7b263ea248452dbd0ae0
César Zárate <cesarjzo@outlook.com>
2023-03-03 22:59:26 -0600

    First attempt

--- march2_exercise/movies_by_gender.r
+++ march2_exercise/movies_by_gender.r
@@ -1,6 +1,6 @@
-# 2. v¿Cuántas películas vieron H y M?
-
 library(dplyr)
+
+# 2. v¿Cuántas películas vieron H y M?
 
 uuser <- read.table("../sources/u.user", header=TRUE, sep="|")
 udata <- read.table("../sources/udata.data", header=TRUE)
@@ -10,7 +10,9 @@ colnames(movies_users)
 
 # get count of column itemid non repeated values
 movies_count <- aggregate(movies_users$userid, by=list(movies_users$itemid), FUN=length)
+movies_count[1:10,]
 colnames(movies_count) <- c("itemid", "count")
+colnames(movies_count)
 
 # count number of movies per user gender
 movies_count <- merge(movies_count, uuser, by.x="itemid", by.y="userid")
--- march2_exercise/places.r
+++ march2_exercise/places.r
@@ -1,3 +1,36 @@
 # ¿Dónde se ven las 10 películas más populares?
 
+library(dplyr)
+
+# read in the data
+udata <- read.table("../sources/udata.data", header=TRUE)
+uitem <- read.table("../sources/u.item", header=TRUE, sep="|")
+
+# merge the data
+movies_users <- merge(udata, uitem, by.x="itemid", by.y="itemid")
+
+# count the number of users per movie
+movies_users_count <- aggregate(movies_users$userid, by=list(movies_users$itemid), FUN=length)
+colnames(movies_users_count) <- c("itemid", "count")
+
+# merge the data again
+users_item <- left_join(movies_users, movies_users_count, by=c("itemid"="itemid"))
+
+# count the number of users per movie
+users_item <- users_item %>% group_by(title) %>% summarise(users=n_distinct(userid))
+
+# sort the data
+users_item <- users_item[order(-users_item$users),]
+
+# get the top 10
+users_item[1:10,]
+
+uuser <- read.table("../sources/u.user", header=TRUE, sep="|")
+
+# get the first character from the zcode column from uuser
+uuser$zcode <- substr(uuser$zcode, 1, 1)
+first_zcode <- uuser$zcode
+colnames(users_item)
+# append first_zcode to users_item
+# users_item$zcode <- first_zcode
 
