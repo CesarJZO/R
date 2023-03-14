102dbb0abd1394c520687a03e7475bd52a89634d
César Zárate <cesarjzo@outlook.com>
2023-03-13 22:51:32 -0600

    Code cleanup

--- unit_3/k-nearest_neighbor.r
+++ unit_3/k-nearest_neighbor.r
@@ -1,5 +1,4 @@
 library(class)
-library(cccd)
 
 # Data frame with height and weight
 data <- data.frame(
@@ -9,17 +8,26 @@ data <- data.frame(
 
 # Adding a column to indicate gender
 data$gender <- c("F", "F", "M", "M", "F", "M")
-data
 
 # Person's height and weight
-new_data <- data.frame(
+new_person <- data.frame(
   height = 1.75,
   weight = 60
 )
 
-graph <- nng(data, k=3)
-plot(graph, data, main="KNN", xlab="Height", ylab="Weight")
+# Predict the gender of the new person
+predicted_gender <- knn(data[,1:2], new_person, data$gender, k=3)
 
-# Predict the gender of the new person based on new_data
-predicted_gender <- class.knn(data[,1:2], new_data, data$gender, k=3)
-predicted_gender
+plot(data$height, data$weight, col = ifelse(data$gender == "M", "blue", "red"), main="K-Vecinos", xlab="Height (m)", ylab="Weight (kg)")
+points(new_person$height, new_person$weight, col = ifelse(predicted_gender == "M", "blue", "red"), pch = 19)
+
+# Format the legend based on the predicted gender
+if(predicted_gender == "M"){
+  legend_text <- c("Female", "Male", "Predicted (M)")
+  legend_col <- c("red", "blue", "blue")
+} else {
+  legend_text <- c("Female", "Male", "Predicted (F)")
+  legend_col <- c("red", "blue", "red")
+}
+
+legend("topleft", legend = legend_text, col = legend_col, pch = c(1, 1, 19))
