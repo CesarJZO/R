8771d20f2e6cd5bb8c51a38de2758a5784f4202b
CesarJZO <cesarjzo@outlook.com>
2023-03-20 20:27:46 -0600

    Semi final attempt

--- exam.r
+++ exam.r
@@ -20,15 +20,43 @@ df <- merge(udata_uitem, uuser, by = "userid")
 library(dplyr)
 library(tidyr)
 
+# Dataset containing only the rate, age and genre columns
 movies_summary <- df %>%
     select(rate, age, Action:Western) %>%
     pivot_longer(
         cols = Action:Western, names_to = "genre", values_to = "genre_value"
     ) %>%
     filter(genre_value == 1) %>%
-    select(-genre_value) %>%
-    group_by(rate, age, genre) %>%
-    summarize(count = n())
+    select(-genre_value)
 
-# to view the summary table
-movies_summary
+movies_summary$genre <- as.factor(movies_summary$genre)
+
+library(ggplot2)
+
+ggplot(movies_summary, aes(x = genre, y = rate, fill = genre)) +
+    geom_boxplot() +
+    labs(
+        x = "Genre",
+        y = "Rate",
+        title = "Boxplot of rate by genre"
+    )
+
+# Plot rate by age using points
+ggplot(movies_summary, aes(x = age, y = rate, fill = genre)) +
+    geom_boxplot() +
+    labs(
+        x = "Age",
+        y = "Rate",
+        title = "Boxplot of rate by age"
+    )
+
+library(MASS)
+
+lda_model <- lda(rate ~ age + genre, data = movies_summary)
+summary(lda_model)
+
+new_data <- data.frame(
+    age = 38, genre = "Comedy"
+)
+predicted_class <- predict(lda_model, newdata = new_data)$class
+predicted_class
