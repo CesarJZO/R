56892f7752734efbad75b72b2df9e2c0da404953
César Zárate <69998059+CesarJZO@users.noreply.github.com>
2023-03-29 21:43:26 +0000

    Add hadoop settings

---
+++ installs/hadoop.sh
@@ -0,0 +1,17 @@
+
+
+# Veryfy, Extract, File
+tar -vxf hadoop-2.6.5.tar.gz
+
+# Move to /usr/local
+# sudo mv hadoop-2.6.5 /usr/local/hadoop
+
+sudo ln -s /u02/hadoop-2.6.5 /usr/local/hadoop
+
+# Add to .bashrc
+export HADOOP_HOME=/usr/local/hadoop
+export PATH=$PATH:$HADOOP_HOME/bin
+export PATH=$PATH:$HADOOP_HOME/sbin
+
+# Show disk usage
+df -h
