ec37e2ad360b206423d6f82ba42cccd98a2bfb98
César Zárate <cesarjzo@outlook.com>
2023-05-14 09:46:02 -0400

    Update hdfs file usage

--- hdfs.r
+++ hdfs.r
@@ -1,6 +1,9 @@
-library(rhdfs)
-
+# Set env if it is not set in your .bashrc
+# Use library(rhdfs) directly otherwise.
+library(rJava)
 Sys.setenv(HADOOP_CMD = "/usr/local/hadoop/bin/hadoop")
+
+library(rhdfs)
 
 hdfs.init()
 
