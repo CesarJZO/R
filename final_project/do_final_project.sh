7d767d20fcc9b48172f58f5e72331c281eaeb7f2
César Zárate <cesarjzo@outlook.com>
2023-05-14 15:12:59 -0400

    Update final project script

--- final_project/do_final_project.sh
+++ final_project/do_final_project.sh
@@ -13,21 +13,13 @@ hdfs dfs -put ./files/tweets.txt /u03/final/
 sqoop export --connect jdbc:mysql://localhost:3306/null_data --password $MYSQL_ROOT_PASS --username root --table Movies --export-dir /u03/final/titles.txt
 
 # Import table from mysql to hdfs
-sqoop import --connect jdbc:mysql://localhost:3306/null_data --password $MYSQL_ROOT_PASS --username root --table Movies -m 1 --target-dir /u03/final/ --direct
+sqoop import --connect jdbc:mysql://localhost:3306/null_data --password $MYSQL_ROOT_PASS --username root --table Movies -m 1 --target-dir /u03/mysql/ --direct
 
 # List items in hdfs
-hdfs dfs -ls /u03/final/
-
-# Specify mapreduce-examples path
-ln -s ln -s /usr/local/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.6.5.jar mapreduce-examples
+hdfs dfs -ls /u03/final/ /u03/mysql/
 
 # Run mapreduce wordcount
-yarn jar mapreduce-examples wordcount \
-/u03/final/part-m-00000 \
-/u03/final/titles.txt \
-/u03/final/titles.json \
-/u03/final/tweets.txt \
-/u03/final_wordcount
+yarn jar mapreduce-examples wordcount /u03/final/* /u03/mysql/part-m-00000 /u03/wordcount
 
 # Run wordcloud script
 R -f ./wordcloud.r
