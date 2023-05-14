2de5938436e2f5a4a160a63b0b4e4ed270c576cb
César Zárate <cesarjzo@outlook.com>
2023-05-14 15:31:20 -0400

    Add tweets file

--- final_project/wordcloud.r
+++ final_project/wordcloud.r
@@ -13,29 +13,33 @@ movies <- hdfs.cat("/u03/final_wordcount/part-r-00000")
 head(movies)
 
 # Corpus vector to be graphed
-mycorpus <- VCorpus(VectorSource(movies))
+clean_movies <- VCorpus(VectorSource(movies))
 
 # Remove keywords
-mycorpus <- tm_map(mycorpus, removeWords, stopwords("english"))
+clean_movies <- tm_map(clean_movies, removeWords, stopwords("english"))
 
 # Remove punctuation manually
 remove_numpunct <- function(x) gsub("[^[:alpha:][:space:]]*", "", x)
-mycorpus <- tm_map(mycorpus, content_transformer(remove_numpunct))
+clean_movies <- tm_map(clean_movies, content_transformer(remove_numpunct))
 
 remove_url <- function(x) gsub("http[^[:space:]]*", "", x)
-mycorpus <- tm_map(mycorpus, content_transformer(remove_url))
+clean_movies <- tm_map(clean_movies, content_transformer(remove_url))
 
 # Remove standard punctuation
-mycorpus <- tm_map(mycorpus, removePunctuation)
-mycorpus <- tm_map(mycorpus, content_transformer(tolower))
-mycorpus <- tm_map(mycorpus, stripWhitespace)
+clean_movies <- tm_map(clean_movies, removePunctuation)
+clean_movies <- tm_map(clean_movies, content_transformer(tolower))
+clean_movies <- tm_map(clean_movies, stripWhitespace)
 
 # Remove specific words
-my_stop_words <- c("the", "ref", "its")
-mycorpus <- tm_map(mycorpus, removeWords, my_stop_words)
+my_stop_words <- c("watch", "the", "ref", "its")
+clean_movies <- tm_map(clean_movies, removeWords, my_stop_words)
+
+# Remove emojis
+remove_emojis <- function(x) gsub("[\U0001F600-\U0001F6FF]", "", x)
+clean_movies <- tm_map(clean_movies, content_transformer(remove_emojis))
 
 wordcloud(
-    mycorpus,
+    clean_movies,
     random.order = FALSE,
     random.color = FALSE,
     colors = brewer.pal(8, "Dark2"),
