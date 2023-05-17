0c860b886ebf9ef6bb9e01833cfe9646360a70a0
Cesar Zárate <cesarjzo@outlook.com>
2023-05-17 07:22:50 -0600

    Add wordcloud test

---
+++ unit_5/wordcloud.r
@@ -0,0 +1,12 @@
+library(NLP)
+library(tm)
+library(RColorBrewer)
+library(wordcloud)
+
+data <- read.table("./sources/udata.data", header=TRUE)
+head(data)
+
+items <- data$itemid
+head(items)
+
+wordcloud(items, random.order=FALSE, random.color=FALSE, colors=brewer.pal(8, "Dark2"))
