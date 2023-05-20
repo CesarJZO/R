09f18ddcfa6bd0c6f683cf17193461eb8ab9fb69
Cesar Zárate <cesarjzo@outlook.com>
2023-05-19 23:27:00 -0600

    Add final lab cleanup file

--- unit_5/wordcloud_clean.r
+++ unit_5/wordcloud_clean.r
@@ -7,15 +7,32 @@ library(wordcloud)
 
 manhattan <- readLines("./sources/manhattan_project.txt")
 
-manhattan <- gsub("[[:punct:]]", "", manhattan)
-manhattan <- gsub("[[:cntrl:]]", "", manhattan)
-manhattan <- gsub("\\d+", "", manhattan)
-manhattan <- gsub("\\s+", " ", manhattan)
+# Corpus vector to be graphed
+mycorpus <- VCorpus(VectorSource(manhattan))
+
+# Remove keywords
+mycorpus <- tm_map(mycorpus, removeWords, stopwords("english"))
+
+# Remove punctuation manually
+remove_NumPunct <- function(x) gsub("[^[:alpha:][:space:]]*", "", x)
+mycorpus <- tm_map(mycorpus, content_transformer(remove_NumPunct))
+
+remove_url <- function(x) gsub("http[^[:space:]]*", "", x)
+mycorpus <- tm_map(mycorpus, content_transformer(remove_url))
+
+# Remove standard punctuation
+mycorpus <- tm_map(mycorpus, removePunctuation)
+mycorpus <- tm_map(mycorpus, content_transformer(tolower))
+mycorpus <- tm_map(mycorpus, stripWhitespace)
+# mycorpus <- tm_map(mycorpus, stemDocument)
+
+# Remove specific words
+myStopWords <- c("the")
+mycorpus <- tm_map(mycorpus, removeWords, myStopWords)
 
 wordcloud(
-    manhattan,
+    mycorpus,
     random.order = FALSE,
     random.color = FALSE,
-    colors = brewer.pal(8, "Dark2")
+    colors = brewer.pal(8, "Dark2"),
 )
-Vcorpus()
